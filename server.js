44a77e48b98c336ed423e65fef3f482ac21229bf
Fathi-Amine <112888632+Fathi-Amine@users.noreply.github.com>
2023-10-06 00:07:56 +0100

    BLOG-5 creating views for authentication and applying register functionality

---
+++ server.js
@@ -0,0 +1,44 @@
+const express = require('express')
+const expressLayout = require('express-ejs-layouts')
+require('dotenv').config();
+const flash = require('connect-flash')
+const session = require('express-session')
+const viewRoutes = require('./Routes/viewRoutes')
+const authRoutes = require('./Routes/authRoutes')
+//App
+
+const app = express();
+//EJS
+app.use(expressLayout)
+app.set('view engine', 'ejs')
+// Body Parser
+app.use(express.urlencoded({ extended: false }));
+
+//expression session middleware
+
+app.use(
+    session({
+        secret: 'secret',
+        resave: true,
+        saveUninitialized: true
+    })
+);
+
+// connect flash
+app.use(flash())
+
+// global variable
+
+app.use((req, res, next)=>{
+    res.locals.success_message = req.flash('success_msg')
+    res.locals.error_message = req.flash('error_msg')
+})
+
+//ROUTES
+app.use('/', viewRoutes)
+app.use('/auth', authRoutes)
+
+const PORT = process.env.PORT
+app.listen(PORT, ()=>{
+    console.log(`listening to port ${PORT} ...`)
+})

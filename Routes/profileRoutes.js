832cd321167886e8051e8a42e31a0155c98344e0
Fathi-Amine <112888632+Fathi-Amine@users.noreply.github.com>
2023-10-09 16:03:59 +0100

    front for details integrated and functionality of delete account added

--- Routes/profileRoutes.js
+++ Routes/profileRoutes.js
@@ -4,12 +4,24 @@ const {ensureAuthentication} = require('../Middlewares/authMiddleware')
 const profileController = require('../Controllers/profileController')
 
 router.get('/pedit', ensureAuthentication, (req, res)=>{
-    res.render('editProfile', {
-        user: req.user
-    })
+    try {
+        const user = req.user;
+        const imageUrl = user.image
+            ? `/images/${user.image}`
+            : '/images/default-profile.png';
+
+        res.render('editProfile', {
+            user: req.user,
+            imageUrl: imageUrl,
+        });
+    } catch (error) {
+        console.error(error);
+    }
 })
 
 
 router.post('/edit', ensureAuthentication, profileController.updateProfile);
+router.post('/delete', ensureAuthentication, profileController.deleteAccount);
+
 
 module.exports = router

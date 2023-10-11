36e1019d12570ee9a2c174f83d67f90b887bc65f
Ielmarzouqy <elmarzouqyimane@gmail.com>
2023-10-11 09:57:27 +0100

    BLOG-13 delete comment modified

--- Controllers/commentController.js
+++ Controllers/commentController.js
@@ -1,11 +1,14 @@
 const Comment = require('../Models/Comment');
+// const csurf = require('csurf');
+// const cookieParser = require('cookie-parser');
+
+
 
 class CommentController {
     static async create(req,res){
         let userId = req.user.id;
         let text = req.body.text;
         let articleId = parseInt(req.params.id);
-
         let comment = new Comment(text,userId,articleId);
         let newComment = await comment.save();
 
--- Routes/commentRoutes.js
+++ Routes/commentRoutes.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const { ensureAuthentication } = require('../Middlewares/authMiddleware')
 const router = express.Router();
+
 const CommentController = require('../Controllers/commentController')
+
 
 router.post('/post/:id', ensureAuthentication, CommentController.create);
 

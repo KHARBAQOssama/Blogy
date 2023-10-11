a42c1fbc97d40ad15bd27b11aa832786e89480b4
Fathi-Amine <112888632+Fathi-Amine@users.noreply.github.com>
2023-10-11 15:53:39 +0100

    updateing structure

--- Routes/profileRoutes.js
+++ Routes/profileRoutes.js
@@ -1,32 +1,15 @@
 const express = require('express')
 const router = express.Router()
-const {ensureAuthentication} = require('../Middlewares/authMiddleware')
-const csrf = require('csurf'); // Import the csurf middleware
+const csrf = require('csurf');
 const cookieParser = require('cookie-parser');
+const {ensureAuthentication} = require('../Middlewares/authMiddleware')
 const profileController = require('../Controllers/profileController')
 
 
 const csrfProtection = csrf({ cookie: true });
 router.use(cookieParser());
-router.get('/pedit', ensureAuthentication, csrfProtection, (req, res)=>{
-    try {
-        const user = req.user;
-        const imageUrl = user.image
-            ? `/img/${user.image}`
-            : '/img/default-profile.png';
-
-        res.render('editProfile', {
-            user: req.user,
-            imageUrl: imageUrl,
-            csrfToken: req.csrfToken(),
-        });
-    } catch (error) {
-        console.error(error);
-    }
-})
-
-
-router.post('/edit', ensureAuthentication, csrfProtection, profileController.updateProfile);
+router.get('/pedit', ensureAuthentication, csrfProtection,profileController.loadEditForm)
+router.post('/edit', ensureAuthentication, profileController.updateProfile);
 router.post('/delete', ensureAuthentication, csrfProtection, profileController.deleteAccount);
 
 
